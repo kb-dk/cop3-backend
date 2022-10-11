7d50733d87794e4539c85759edc9379407df869b
David Grove Jørgensen <dgj@kb.dk>
2022-10-11 10:47:22 +0200

    migrate types

--- src/main/java/dk/kb/cop3/backend/migrate/ObjectConverter.java
+++ src/main/java/dk/kb/cop3/backend/migrate/ObjectConverter.java
@@ -2,9 +2,15 @@ package dk.kb.cop3.backend.migrate;
 
 import dk.kb.cop3.backend.crud.database.hibernate.Category;
 import dk.kb.cop3.backend.crud.database.hibernate.Edition;
+import dk.kb.cop3.backend.crud.database.hibernate.Object;
+import dk.kb.cop3.backend.crud.database.hibernate.Type;
+import dk.kb.cop3.backend.crud.database.type.JGeometryType;
+import dk.kb.cop3.backend.crud.database.type.Point;
 import dk.kb.cop3.backend.migrate.hibernate.CategoryOracle;
 import dk.kb.cop3.backend.migrate.hibernate.CommentOracle;
 import dk.kb.cop3.backend.migrate.hibernate.EditionOracle;
+import dk.kb.cop3.backend.migrate.hibernate.ObjectOracle;
+import dk.kb.cop3.backend.migrate.hibernate.TypeOracle;
 
 import java.util.HashSet;
 
@@ -46,4 +52,45 @@ public class ObjectConverter {
         category.setCategoryText(categoryOracle.getCategoryText());
         return category;
     }
+
+    public static Type convertType(TypeOracle oraType) {
+        Type type = new Type();
+        type.setId(oraType.getId());
+        type.setTypeText(oraType.getTypeText());
+        return type;
+    }
+
+    public static Object convertObject(ObjectOracle oraObject) {
+        Object object = new Object();
+        object.setId(oraObject.getId());
+        object.setEdition(convertEdition(oraObject.getEdition()));
+        object.setMods(oraObject.getMods());
+        object.setLastModified(oraObject.getLastModified());
+        object.setDeleted(oraObject.getDeleted()==0x00?'n': oraObject.getDeleted());
+        object.setLastModifiedBy(oraObject.getLastModifiedBy());
+        object.setType(convertType(oraObject.getType()));
+        object.setObjVersion(oraObject.getObjVersion());
+        object.setPoint(convertPoint(oraObject.getPoint()));
+        object.setTitle(oraObject.getTitle());
+        object.setCreator(oraObject.getCreator());
+        object.setRandomNumber(oraObject.getRandomNumber());
+        object.setInterestingess(oraObject.getInterestingess());
+        object.setPerson(oraObject.getPerson());
+        object.setBuilding(oraObject.getBuilding());
+        object.setLocation(oraObject.getLocation());
+        object.setNotBefore(oraObject.getNotBefore());
+        object.setNotAfter(oraObject.getNotAfter());
+        object.setCorrectness(oraObject.getCorrectness());
+        object.setLikes(oraObject.getLikes());
+        object.setBookmark(oraObject.getBookmark());
+        return object;
+    }
+
+    private static Point convertPoint(JGeometryType point) {
+        if (point == null) {
+            return null;
+        }
+        return new Point(point.getPoint()[0],point.getPoint()[1]);
+    }
+
 }
