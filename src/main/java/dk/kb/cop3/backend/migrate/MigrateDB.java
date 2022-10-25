fb1894ad9b6fca81c31c3db838c8d349fb0e1436
Zahra <zahramousavidk@gmail.com>
2022-10-25 06:45:42 -0700

    Added a class to create and migrate the database

---
+++ src/main/java/dk/kb/cop3/backend/migrate/MigrateDB.java
@@ -0,0 +1,16 @@
+package dk.kb.cop3.backend.migrate;
+
+public class MigrateDB {
+    public static void main(String[] args) {
+        CreateDB.main(args);
+        MigrateTags.main(args);
+        MigrateEditions.main(args);
+        MigrateType.main(args);
+        MigrateXLink.main(args);
+        MigrateCategories.main(args);
+        MigrateUserRole.main(args);
+        MigrateUsers.main(args);
+        MigrateObjects.main(args);
+        MigrateComments.main(args);
+    }
+}
