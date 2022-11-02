16d87cb71a239985ce8f42268a98294d7ab6e296
David Grove Jørgensen <dgj@kb.dk>
2022-11-02 14:40:45 +0100

    CreateService add GET function

--- src/main/java/dk/kb/cop3/backend/crud/api/CreateService.java
+++ src/main/java/dk/kb/cop3/backend/crud/api/CreateService.java
@@ -17,6 +17,7 @@ import org.hibernate.Transaction;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.net.URI;
 
@@ -28,6 +29,37 @@ public class CreateService {
     private static Logger logger = Logger.getLogger(CreateService.class);
     private CopBackendProperties consts = CopBackendProperties.getInstance();
 
+    @GET
+    @Path("/{medium}/{collection}/{year}/{month}/{edition}/{id}")
+    public Response createCobjectFromMods(@PathParam("medium") String medium,
+                                          @PathParam("collection") String collection,
+                                          @PathParam("year") int year,
+                                          @PathParam("month") String month,
+                                          @PathParam("edition") String edition,
+                                          @PathParam("id") String id) {
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        String idFromRequest = "/" + medium + "/" + collection + "/" + year + "/" + month + "/" + edition + "/" + id;
+        try {
+            Transaction tx = session.beginTransaction();
+            Object object = session.get(Object.class,idFromRequest);
+            tx.commit();
+            if (object != null) {
+                return Response.ok(object.getMods())
+                        .header("Last-Modified-Time-Stamp",object.getLastModified())
+                        .type(MediaType.APPLICATION_XML_TYPE)
+                        .build();
+            }
+            return Response.status(HttpStatus.SC_NOT_FOUND).build();
+        } catch (Exception e) {
+            logger.error("Error getting object",e);
+            return Response.serverError().build();
+        } finally {
+            session.close();
+        }
+    }
+    
+    
     /**
      * Put-service. Receive som data, and try saving it to db.
      * PUT http://localhost:8080/cop/syndication/SOMEOBJECT (the path is not final)
