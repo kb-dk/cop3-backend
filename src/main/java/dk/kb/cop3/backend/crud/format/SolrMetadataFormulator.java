40a8266b6d0dc26085fc587910e9cec7c19e6045
David Grove Jørgensen <dgj@kb.dk>
2025-03-04 17:00:03 +0100

    added option to trace debug transformations

--- src/main/java/dk/kb/cop3/backend/crud/format/SolrMetadataFormulator.java
+++ src/main/java/dk/kb/cop3/backend/crud/format/SolrMetadataFormulator.java
@@ -3,6 +3,9 @@ package dk.kb.cop3.backend.crud.format;
 import dk.kb.cop3.backend.constants.CopBackendProperties;
 
 import dk.kb.cop3.backend.crud.util.TransformErrorListener;
+import org.apache.xalan.trace.*;
+import org.apache.xalan.transformer.TransformerImpl;
+import org.apache.xml.serializer.SerializerTrace;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
@@ -10,8 +13,10 @@ import org.w3c.dom.Element;
 
 import javax.xml.transform.ErrorListener;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.dom.DOMResult;
 import javax.xml.transform.dom.DOMSource;
+import java.util.TooManyListenersException;
 
 /**
  * Reads a MetadataSource returning mods data, creating SOLR an add document
@@ -46,7 +51,14 @@ public class SolrMetadataFormulator extends MetadataFormulator {
         DOMSource dom_source = new DOMSource(src);
         DOMResult dom_result = new DOMResult();
 
-        Transformer mods2solr = this.trInit("/mods_solrizr.xsl");
+        Transformer mods2solr = null;
+        try {
+            mods2solr = this.trInit("/mods_solrizr.xsl");
+        } catch (TooManyListenersException e) {
+            throw new RuntimeException(e);
+        }
+
+
         mods2solr.setErrorListener(new TransformErrorListener("mods_solrizr.xsl"));
         mods2solr.setParameter("metadata_context", copBaseUrl);
         mods2solr.setParameter("url_prefix", baseUrl);
@@ -63,7 +75,7 @@ public class SolrMetadataFormulator extends MetadataFormulator {
         return solr_doc;
     }
 
-    private javax.xml.transform.Transformer trInit(java.lang.String xsl) {
+    private javax.xml.transform.Transformer trInit(java.lang.String xsl) throws TooManyListenersException {
 
         String xfile = xsl;
         // getResource() returns a file URL
@@ -83,6 +95,75 @@ public class SolrMetadataFormulator extends MetadataFormulator {
         if (transform == null) {
             logger.debug("transform for " + xfile + " didn't work as expected");
         }
+
+        // Enable tracing
+        if (CopBackendProperties.getTraceTransforms()) {
+            TraceListener traceListener = new TraceListener() {
+                @Override
+                public void trace(TracerEvent ev) {
+                    logger.info("==== XSLT Execution Trace ====");
+                    logger.info("XSLT Node: " + ev.m_styleNode.getNodeName());
+                    logger.info("XML Source Node: " +
+                            (ev.m_sourceNode != null ? ev.m_sourceNode.getNodeName() : "N/A"));
+                    logger.info("XML Source Node attributes: " +
+                            (ev.m_sourceNode != null && ev.m_sourceNode.getAttributes() != null ? ev.m_sourceNode.getAttributes().getLength() : "N/A"));
+                    logger.info("Line Number: " + ev.m_styleNode.getLineNumber());
+                    logger.info("Column Number: " + ev.m_styleNode.getColumnNumber());
+                    logger.info("=============================");
+                }
+
+                @Override
+                public void selected(SelectionEvent ev) throws TransformerException {
+                    logger.info("XSLT Selection Event:");
+                    logger.info("Expression: " + ev.m_xpath.getPatternString());
+                }
+
+                @Override
+                public void generated(GenerateEvent event) {
+                    switch (event.m_eventtype) {
+                        case SerializerTrace.EVENTTYPE_STARTDOCUMENT:
+                            logger.info("Start of document");
+                            break;
+
+                        case SerializerTrace.EVENTTYPE_ENDDOCUMENT:
+                            logger.info("End of document");
+                            break;
+
+                        case SerializerTrace.EVENTTYPE_STARTELEMENT:
+                            logger.info("Start Element: <" + event.m_name + ">");
+                            break;
+
+                        case SerializerTrace.EVENTTYPE_ENDELEMENT:
+                            logger.info("End Element: </" + event.m_name + ">");
+                            break;
+
+                        case SerializerTrace.EVENTTYPE_CHARACTERS:
+                            if (event.m_characters != null) {
+                                logger.info("Text Node: " + new String(event.m_characters, event.m_start, event.m_length));
+                            }
+                            break;
+
+                        case SerializerTrace.EVENTTYPE_CDATA:
+                            if (event.m_characters != null) {
+                                logger.info("CDATA: " + new String(event.m_characters, event.m_start, event.m_length));
+                            }
+                            break;
+
+                        case SerializerTrace.EVENTTYPE_COMMENT:
+                            if (event.m_characters != null) {
+                                logger.info("Comment: " + new String(event.m_characters, event.m_start, event.m_length));
+                            }
+                            break;
+
+                        default:
+                            logger.info("Unhandled GenerateEvent type: " + event.m_eventtype);
+                    }
+                }
+
+            };
+            ((TransformerImpl) transform).getTraceManager().addTraceListener(traceListener);
+        }
+
         return transform;
     }
 
