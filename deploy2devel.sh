f52e9271295af896ccff9c8ee031c05b202e3f79
David Grove Jørgensen <dgj@kb.dk>
2025-06-13 09:32:18 +0200

    java 11

--- deploy2devel.sh
+++ deploy2devel.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo 'Building war file'
-mvn -DskipTests clean package
+JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64 mvn -DskipTests clean package
 echo 'Deploying war to cop-devel-01.kb.dk'
 scp target/cop3-backend*.war cop@cop-devel-01:services/webapps/cop.war
 echo 'Done'
